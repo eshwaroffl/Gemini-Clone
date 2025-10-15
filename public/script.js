f369e20d2f727f0748b5d3eda05ccd4c3525b6bc
eshwaroffl <eshwaroffl15@gmail.com>
2025-10-15 20:15:31 +0530

    fix : fixed error

--- public/script.js
+++ public/script.js
@@ -7,8 +7,8 @@ const deleteChatButton = document.querySelector("#delete-chat-button");
 let userMessage = null;
 let isResponseGenerating = false;
 // API configuration
-const API_KEY = 'AIzaSyDe7uxvGbqnDX9DN8hWWm3M-A8anRmbj_U'; // Your API key here
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
+const API_KEY = 'AIzaSyBu2STyJqnp3N81J1WkFu3qh9k6b7qzwww'; // Your API key here
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 // Load theme and chat data from local storage on page load
 const loadDataFromLocalstorage = () => {
     const savedChats = localStorage.getItem("saved-chats");
